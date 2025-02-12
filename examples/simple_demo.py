ac5e2475079bed3e82f044689729d92f841091b6
Jack <jckwind11@gmail.com>
2025-02-12 17:41:31 -0500

    .

--- examples/simple_demo.py
+++ examples/simple_demo.py
@@ -14,7 +14,7 @@ model_path = "meta-llama/Llama-3.2-1B-Instruct"
 tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
 model = PSE_Torch.from_pretrained(
     model_path,
-    torch_dtype=torch.float16,
+    torch_dtype=torch.bfloat16,
     device_map="auto",
 )
 
@@ -43,6 +43,7 @@ input_ids = tokenizer.apply_chat_template(
 assert isinstance(input_ids, torch.Tensor)
 input_ids = input_ids.to(model.device)
 assert isinstance(input_ids, torch.Tensor)
+print(repr(model.engine))
 output = model.generate(
     input_ids,
     do_sample=True,
